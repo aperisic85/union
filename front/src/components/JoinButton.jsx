3bcaeb9ae39a4536fce0a9368eff6158599d0318
Ante Perisic lap <perisic.ante@gmail.com>
2025-03-12 21:28:35 +0100

    add Link to join button

--- front/src/components/JoinButton.jsx
+++ front/src/components/JoinButton.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import "./JoinButton.css"; // CSS datoteka za stilizaciju
+import { Link } from "react-router-dom"; // Uvoz Link komponente
 
 const JoinButton = () => {
   return (
     <div className="join-container">
-      <a href="/join" className="join-button">
+      <Link to="/join" className="join-button">
+        {" "}
+        {/* Koristimo Link komponentu */}
         Učlani se u sindikat!
-      </a>
+      </Link>
     </div>
   );
 };
