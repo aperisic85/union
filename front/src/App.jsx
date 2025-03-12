1ce598ec87d83464375692c9e5845bea980c8909
Ante Perisic lap <perisic.ante@gmail.com>
2025-03-12 16:53:03 +0100

    clean add news

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import NewsDetail from "./pages/NewsDetail";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import About from "./pages/About";
-import AddNews from "./pages/AddNews";
 import Contact from "./pages/Contact";
 
 function App() {
@@ -15,9 +13,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/news/:id" element={<NewsDetail />} />
           <Route path="/about" element={<About />} />
-          <Route path="/add" element={<AddNews />}></Route>
           <Route path="/contact" element={<Contact />}></Route>
           {/* Ostale rute */}
         </Routes>
--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
-import NewsList from '../components/NewsList';
+import React from "react";
+import "./Home.css"; // Uvezite CSS datoteku
+import slika1 from "../assets/images/slika_1.jpeg";
 
-const Home = ({ news }) => {
+const Home = ({}) => {
+  const mockNews = [
+    {
+      id: 1,
+      title: "Novosti",
+      description: "Ovdje će biti novosti",
+      image: { slika1 },
+    },
+    {
+      id: 2,
+      title: "Novosti",
+      description: "Ovdje će biti novosti",
+      image: "https://via.placeholder.com/150",
+    },
+    {
+      id: 3,
+      title: "Novosti",
+      description: "Ovdje će biti novosti",
+      image: "https://via.placeholder.com/150",
+    },
+  ];
+
   return (
-    <div className="page-content container">
+    <div className="home-container">
+      {" "}
+      {/* Promijenjeno ime klase */}
       <h1>Novosti</h1>
-      <NewsList news={news} />
+      <button className="load-more">Prikaži još vijesti</button>
     </div>
   );
 };
