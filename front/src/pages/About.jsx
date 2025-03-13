361b6e464366cde723217340742468a9bc5c2e20
Ante Perisic <perisic.ante@gmail.com>
2025-03-13 23:52:30 +0100

    fix About

--- front/src/pages/About.jsx
+++ front/src/pages/About.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./About.css"; // CSS za stilizaciju
+import ReactMarkdown from "react-markdown";
+import { Link } from "react-router-dom";
 
 const About = () => {
   return (
@@ -10,28 +12,28 @@ const About = () => {
         {" "}
         {/* Stilski kontejner */}
         <h1>O nama</h1>
-        <p>
+        <ReactMarkdown>
           Sindikat zaposlenika Plovput je organizacija koja predstavlja i brine
-          o pravima i interesima svih zaposlenika u Plovputu. Naš cilj je
-          poboljšati radne uvjete, zaštititi prava zaposlenika i osigurati bolju
+          o **pravima** i **interesima** svih zaposlenika u Plovputu. Naš cilj je
+          **poboljšati** radne uvjete, **zaštititi** prava zaposlenika i **osigurati** bolju
           budućnost za sve članove.
-        </p>
-        <p>
-          Kao Sindikat, aktivno radimo na promociji socijalne pravde,
-          transparentnosti i demokratskog upravljanja u našoj organizaciji. Naša
-          misija je da omogućimo zaposlenicima da imaju glas i da budu aktivni
+        </ReactMarkdown>
+        <ReactMarkdown>
+          Kao Sindikat, aktivno radimo na promociji **socijalne pravde** ,
+          transparentnosti i demokratskog upravljanja u našoj organizaciji.Naša
+          **misija** je da omogućimo zaposlenicima da imaju glas i da budu aktivni
           uključeni u odluke koje utječu na njihove karijere i živote.
-        </p>
-        <p>
-          Naš tim se sastoji od predanih i iskusnih ljudi koji su posvećeni
-          našoj zajednici. Radimo u skladu s načelima solidarnosti, poštenja i
+          </ReactMarkdown>
+          <p>
+          <Link to={"/contact"}>Naš tim</Link> se sastoji od predanih i iskusnih ljudi koji su posvećeni
+          našoj zajednici.Radimo u skladu s načelima solidarnosti, poštenja i
           profesionalnosti, kako bismo osigurali da svi zaposlenici imaju
           sigurno i podržavajuće radno okruženje.
-        </p>
+          </p>
         <p>
           Ako ste zainteresirani za pridruživanje našoj zajednici ili imate
           pitanja, molimo vas da nas{" "}
-          <a href="mailto:info@sindikatplovputa.hr">kontaktirate</a>. Spremni
+          <a href="mailto:info@sindikatplovputa.hr">kontaktirate</a>.Spremni
           smo vam pomoći i podržati vas u svim pitanjima vezanim za rad i
           Sindikat.
         </p>
